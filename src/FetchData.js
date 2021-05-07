b0684984475674fddc1d9a2a5e95b417d5500f4d
Omar Hussein <omarhussein@omars-mbp.home>
2021-05-07 06:04:04 +0100

    some component styling, attempt at updating total amount available for investment using state

--- src/FetchData.js
+++ src/FetchData.js
@@ -6,24 +6,46 @@ const FetchData = () => {
   const [invest, setInvest] = useState(false);
   const [investAvailable, setInvestAvialable] = useState(false)
   const [buttonPopup, setButtonPopup] = useState(false)
+  const [investCard, setInvestCard] = useState("")
+  const [totalAmountAvailable, setTotalAmountAvailable] = useState(0)
+  let totalValue = 0;
 
-
+    function showMe(val) {
+      val.stopPropagation();
+      setButtonPopup(true);
+      setInvestCard("active")
+      setTotalAmountAvailable(getTotalAmountAvailable());
+    }
     function getData(val){
       setData(val.target.value);
       setInvest(false);
       setInvestAvialable(false);
     }
-    let totalValue = 0;
+
+    function getTotalAmountAvailable(){
+      
+      {InvestData.forEach(function(e){
+        let totalAmount = e.amount.replace(/\,/g, '')
+        let parseAmount = parseInt(totalAmount)
+        totalValue += parseAmount
+      })}
+      console.log(totalValue)
+      return totalValue
+    }
+    getTotalAmountAvailable();
+   
         return (
           <div className="main">
             <div className="header"> <h1>Current Loans</h1> </div>
             {InvestData.map((investDetails, index ) => {
               return(
-                <div className={"LoanBox " + investDetails.tranche} key={investDetails.id}>
+                <div className={"loanBox " + investDetails.tranche + " " + investCard} key={investDetails.id}>
                     <div className="content">
+                      <div className="details">
                         <h4>Loan Name {index+1}</h4>
                         <p>Loan details, amounts and values</p>
-                        <button onClick={() => setButtonPopup(true)}>INVEST</button>
+                      </div>
+                        <button onClick={showMe}>INVEST</button>
                       </div>
                   </div>
                 )
@@ -54,14 +76,14 @@ const FetchData = () => {
                   })}
 
           </PopUp>
-              {InvestData.forEach(function(e){
+              {/* {InvestData.forEach(function(e){
                 let totalAmount = e.amount.replace(/\,/g, '')
                 let parseAmount = parseInt(totalAmount)
                 totalValue += parseAmount
-              })}
+              })} */}
 
 
-            <div className="totalAvailable"> Total amount avilable for investments: <span>£{totalValue}</span></div>
+            <div className="totalAvailable"> Total amount avilable for investments: <span>£{totalAmountAvailable}</span></div>
           
           </div>
         );
